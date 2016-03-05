ee296a3bf78de942cb878a040aaa164e07677a2e
Bart Maertens <bart.maertens@know.bi>
2016-03-05 15:40:20 +0100

    Initial version

---
+++ src/main/java/bi/know/kettle/neo4j/out/Neo4JOutput/Neo4JOutputData.java
@@ -0,0 +1,14 @@
+package bi.know.kettle.neo4j.out.Neo4JOutput;
+
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.trans.step.BaseStepData;
+import org.pentaho.di.trans.step.StepDataInterface;
+
+public class Neo4JOutputData extends BaseStepData implements StepDataInterface{
+	public RowMetaInterface outputRowMeta;
+	
+	public Neo4JOutputData(){
+		super();
+	}
+
+}
